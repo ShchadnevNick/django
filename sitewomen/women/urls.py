f7967fbfb9668e9eb38e5ac9a92cc32949063813
Nicolay <nicolay-shchadnev@mail.ru>
2025-01-24 18:03:30 +0500

    Creating template tags - simple_tag, inclusion_tag

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -1,4 +1,5 @@
 from multiprocessing.resource_tracker import register
+from tkinter.font import names
 
 from django.urls import path, re_path, register_converter
 from women import views
@@ -7,10 +8,11 @@ from women import converters
 
 register_converter(converters.FourDigitYearConverter, 'year4')
 
-
 urlpatterns = [
     path('', views.index, name='home'),
     path('about/', views.about, name='about'),
-    path('cats/<slug:cat_slug>', views.categories, name='cats'),
-    path('archive/<year4:year>/', views.archive, name='archive')
+    path('addpage/', views.addpage, name='add_page'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login, name='login'),
+    path('post/<int:post_id>/', views.show_post, name='post'),
 ]
