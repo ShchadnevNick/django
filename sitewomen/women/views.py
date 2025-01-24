0afa4cea0d000f3043a9ee6383bf9674afab16af
Nicolay <nicolay-shchadnev@mail.ru>
2025-01-24 15:28:37 +0500

    Dynamic urls and converters

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return HttpResponse('<h1>Главая страница</h1>')
+
+def about(request):
+    return HttpResponse('О сайте')
