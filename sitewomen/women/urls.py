850dc90fba6dc675bc8d0627489fcdf66b1f0923
Nicolay <nicolay-shchadnev@mail.ru>
2025-01-24 15:41:52 +0500

    Exception handling for requests to the server

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -1,10 +1,16 @@
-from django.urls import path, re_path
+from multiprocessing.resource_tracker import register
+
+from django.urls import path, re_path, register_converter
 from women import views
+from women import converters
+
+
+register_converter(converters.FourDigitYearConverter, 'year4')
 
 
 urlpatterns = [
     path('', views.index, name='home'),
     path('about/', views.about, name='about'),
     path('cats/<slug:cat_slug>', views.categories, name='cats'),
-    re_path(r'^archive/(?P<year>[0-9]{4})/', views.archive, name='archive')
+    path('archive/<year4:year>/', views.archive, name='archive')
 ]
