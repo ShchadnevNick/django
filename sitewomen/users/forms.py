8a2ca6c38fd2687ad0363a5b452d908e1a8c3c19
Nicolay <nicolay-shchadnev@mail.ru>
2025-04-19 23:25:46 +0500

    Extension of the User model. AbstractUser class

--- sitewomen/users/forms.py
+++ sitewomen/users/forms.py
@@ -1,7 +1,8 @@
+import datetime
+
 from django import forms
-from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, PasswordChangeForm
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 
 class LoginUserForm(AuthenticationForm):
@@ -9,7 +10,7 @@ class LoginUserForm(AuthenticationForm):
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
 
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ['username', 'password']
 
 
@@ -41,6 +42,7 @@ class RegisterUserForm(UserCreationForm):
 
     def clean_email(self):
         email = self.cleaned_data['email']
+        User = get_user_model()  # Получаем актуальную модель
         if User.objects.filter(email=email).exists():
             raise forms.ValidationError("Такой E-mail уже существует!")
         return email
@@ -49,10 +51,12 @@ class RegisterUserForm(UserCreationForm):
 class ProfileUserForm(forms.ModelForm):
     username = forms.CharField(disabled=True, label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
     email = forms.CharField(disabled=True, label='E-mail', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    this_year = datetime.date.today().year
+    date_birth = forms.DateField(widget=forms.SelectDateWidget(years=tuple(range(this_year - 100, this_year - 5))))
 
     class Meta:
         model = get_user_model()
-        fields = ['username', 'email', 'first_name', 'last_name']
+        fields = ['photo', 'username', 'email', 'date_birth', 'first_name', 'last_name']
         labels = {
             'first_name': 'Имя',
             'last_name': 'Фамилия',
